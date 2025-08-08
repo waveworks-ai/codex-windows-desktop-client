f02f72424627cec7d6921f689773947cc6b9fdcb
Gustaf Hammarberg <gustaf@waveworks.dk>
2025-08-08 16:01:48 +0200

    Fixed build error.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace codex_windows_desktop_client
+﻿namespace codex_windows_desktop_client
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
     }
 
