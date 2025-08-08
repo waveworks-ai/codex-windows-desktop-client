21e1355c5b22557a63416fd1cc2ad4e4c5c45745
Gustaf Hammarberg <gustaf@waveworks.dk>
2025-08-08 14:01:51 +0200

    Initial commit of empty .NET WPF solution.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace codex_windows_desktop_client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
